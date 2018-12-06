193f1e2a24811d17d2920388eb54b437749265ff
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-06 20:55:03 +0800

    Brew formula update for ya version v0.0.3

---
+++ ya.rb
@@ -0,0 +1,11 @@
+class Ya < Formula
+  desc ""
+  homepage "https://github.com/codeskyblue/ya"
+  url "https://github.com/codeskyblue/ya/releases/download/v0.0.3/ya_0.0.3_darwin_amd64.zip"
+  version "0.0.3"
+  sha256 "f5579d990da55a7c9ae36fc35abbc51cae3eea946f4429b7990862bb4ee19ad0"
+
+  def install
+    bin.install "ya"
+  end
+end
