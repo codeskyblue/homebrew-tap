bdbeecfb0b74e8df47de80dd30f44ead79985363
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-11 13:55:58 +0800

    Brew formula update for fa version v1.0.0

--- fa.rb
+++ fa.rb
@@ -1,9 +1,9 @@
 class Fa < Formula
   desc ""
   homepage ""
-  url "https://github.com/codeskyblue/fa/releases/download/v0.1.1/fa_0.1.1_darwin_amd64.zip"
-  version "0.1.1"
-  sha256 "18c974b2e7682da06badb896c97a434257a4718f9db3da2202180525c78ac914"
+  url "https://github.com/codeskyblue/fa/releases/download/v1.0.0/fa_1.0.0_darwin_amd64.zip"
+  version "1.0.0"
+  sha256 "8cc59d36c2dc2e078a47bcbb0a1da949ed704311028db37c2bef862fbb0a8bf2"
 
   def install
     bin.install "fa"
